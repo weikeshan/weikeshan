03ee645654732ce443fa087dfac406b1ce84315c
weikeshan <398043454@qq.com>
2016-10-09 19:49:19 +0800

    Update wkscool.js

--- javascripts/wkscool.js
+++ javascripts/wkscool.js
@@ -220,7 +220,7 @@ function ADDLRC(text){
         fragment.appendChild(line);
 	}
 	lrc.appendChild(fragment);
-	$('#lrc').css( 'height', window.innerHeight-90);
+	$('#lrc').css( 'height', window.innerHeight/8);
 	zxx=document.getElementById("lrcc").offsetTop;
 	lines=text.length;//63
 	lineH=document.getElementById("lrcc").offsetHeight/lines;//40
